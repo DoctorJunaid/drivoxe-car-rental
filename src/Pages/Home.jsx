b22cb4ce52aaf03eabdf61f16c495e530f546b36
DoctorJunaid <junaid.aurangzeb1@gmail.com>
2025-10-09 20:34:30 +0500

    second

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../Components/Navbar.jsx";
 import Hero from "@/Components/Hero.jsx";
 import Carousal from "@/Components/Carousal.jsx";
 import BuySection from "@/Components/BuySection.jsx";
+import AnimatedTestimonialsData from "@/Components/AnimatedTestimonialsData.jsx";
 
 // A simple utility to get the base image URL
 
@@ -15,6 +16,9 @@ const Home = () => {
             <Hero />
             <Carousal />
             <BuySection />
+            <AnimatedTestimonialsData />
+
+
 
 
         </div>
